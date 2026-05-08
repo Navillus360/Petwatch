a3b6d0f5e91b5a44456cf573cd57f7445cff13df
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2026-05-08 13:44:28 +0100

    Schema location bug
    Fixed a bug when executing the setup script where the schema.sql file could not be located and would result in an empty sqlite database.

--- database/setup.php
+++ database/setup.php
@@ -2,6 +2,6 @@
 require_once __DIR__ . '/../app/models/Database.php';
 use App\Models\Database;
 $db = Database::connect();
-$schema = file_get_contents(__DIR__ . 'database/schema.sql');
+$schema = file_get_contents(__DIR__ . '/schema.sql');
 $db->exec($schema);
 echo "Database schema created successfully!";
